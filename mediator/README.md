4b44b5eb556d40d9a4784b9d449b03ac24ed14c8
Igor Bronowicki <ibronovytskyi@ebay.com>
2013-08-08 10:47:05 +0300

    ***

---
+++ mediator/README.md
@@ -0,0 +1,5 @@
+# mediator
+
+1. First be sure you have Node.js installed.
+2. $ npm install
+3. $ node app.js
---
+++ mediator/db.js
@@ -0,0 +1,14 @@
+exports.getPages = function() {
+    return {
+        "pages": [
+            {
+                "author": "Dennis",
+                "title": "Sample Page"
+            },
+            {
+                "author": "Dennis",
+                "title": "Terms of Services"
+            }
+        ]
+    };
+}
