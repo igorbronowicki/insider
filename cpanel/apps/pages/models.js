a3184e34667021ba11de567e6f5012871fd8e936
Igor Bronowicki <ibronovytskyi@ebay.com>
2013-08-10 15:55:56 +0300

    ***

---
+++ cpanel/apps/pages/models.js
@@ -0,0 +1,72 @@
+var pages = exports.pages = {
+    _validate: function(ob) {
+        return {
+            "errors": {
+                "title": [
+                    "can't be blank"
+                ]
+            }
+        };
+    },
+
+
+    create: function(ob) {
+        // code MongoDB
+        return {
+            "page": ob
+        };
+    },
+
+
+    read: function(ob) {
+        if (ob) {
+            return {
+                "page": {
+                    "author": "Dennis",
+                    "title": "Sample Page",
+                    "URL": "",
+                    "visibility": "",
+                    "template": ""
+                }
+            };
+        } else {
+            return {
+                "pages": [
+                    {
+                        "author": "Dennis",
+                        "title": "Sample Page",
+                        "URL": "",
+                        "visibility": "",
+                        "template": ""
+                    },
+                    {
+                        "author": "Igor",
+                        "title": "Sample Page2",
+                        "URL": "",
+                        "visibility": "",
+                        "template": ""
+                    },
+                    {
+                        "author": "Dennis",
+                        "title": "Terms of Services",
+                        "URL": "",
+                        "visibility": "",
+                        "template": ""
+                    }
+                ]
+            };
+        }
+    },
+
+
+    update: function(ob) {
+        return {
+            "page": ob
+        };
+    },
+
+
+    delete: function(id) {
+        return {};
+    }
+};
---
+++ cpanel/public/app.js
@@ -0,0 +1,43 @@
+$(function(){
+    window.pages = {
+        el: $("#pages"),
+        template: $("#tpl-pages").html(),
+        model: [],
+        init: function() {
+            this.events();
+        },
+        events: function() {
+            var self = this;
+
+            $("button").click(function(){
+                $.ajax({
+                    url: "http://127.0.0.1:3000/v2/pages.json"
+                }).done(function(data) {
+                        self.updateModel(data);
+                    });
+            });
+        },
+        updateModel: function(data) {
+            this.model = data;
+            this.render();
+        },
+        render: function() {
+            this.el.html(Mustache.render(this.template, this.model));
+        },
+        empty: function() {
+            this.el.empty();
+        }
+    };
+
+
+
+    pages.init();
+
+
+
+
+});
+
+
+
+
